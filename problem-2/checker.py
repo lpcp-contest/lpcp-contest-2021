2023e05344f7478707054509f081ec608b34578d
Mario Alviano <mario.alviano@gmail.com>
2021-09-24 20:58:45 +0200

    Oops! Revert checker 3. Fix checker *2*

--- problem-2/checker.py
+++ problem-2/checker.py
@@ -2,57 +2,58 @@
 """
 Example input:
 
-5 6
-1 1 1 2 3
-4 3 1 3 3
-5 4 5 2 5
-1 6 6 2 6
-5 5 5 4 2
-11
-4 4 5 5
-4 2 4 5
-2 1 5 4
-1 5 2 4
-3 1 3 3
-1 1 1 3
-2 3 4 1
-2 2 2 5
-3 5 5 3
-1 4 3 4
-5 1 5 2
+3
+20 11 21
+41 41 10
+32 52 22
+15
+1 1
+1 1
+1 1
+1 2
+1 3
+1 3
+1 3
+2 1
+2 1
+2 3
+3 1
+3 1
+3 3
+3 3
+3 3
 """
 import fileinput #line:26
 import re #line:27
 import sys #line:28
-def pretty_print (OOO00OO000OO0O00O ,OOOOO000OO00OO0OO ,cut =None ):#line:31
-    if len(sys.argv) >= 2 and sys.argv[1] == '--quiet': return
+def pretty_print (O0000OO00OO0O00O0 ,OO0O0OO0OOOO000O0 ,cut =None ):#line:31
     if cut is None :#line:32
         cut =[]#line:33
-    print (f'#### STEP {OOO00OO000OO0O00O} ####')#line:34
-    for OO0OO0O00OOO0OO00 in range (1 ,len (OOOOO000OO00OO0OO )):#line:35
-        OO0OO0O000O00O00O =[]#line:36
-        for O0OO0OOOOO0000000 in range (1 ,len (OOOOO000OO00OO0OO [OO0OO0O00OOO0OO00 ])):#line:37
-            O0000O00O000OO0O0 =OOOOO000OO00OO0OO [OO0OO0O00OOO0OO00 ][O0OO0OOOOO0000000 ]if OOOOO000OO00OO0OO [OO0OO0O00OOO0OO00 ][O0OO0OOOOO0000000 ]!=0 else '.'#line:38
-            if (OO0OO0O00OOO0OO00 ,O0OO0OOOOO0000000 )in cut :#line:39
-                OO0OO0O000O00O00O .append (f"[{O0000O00O000OO0O0}]")#line:40
+    print (f'#### STEP {O0000OO00OO0O00O0} ####')#line:34
+    for OOO0O0O0000OOO0OO in range (1 ,len (OO0O0OO0OOOO000O0 )):#line:35
+        O0OOO000OOOOO0O0O =[]#line:36
+        for O00O000000O00O000 in range (1 ,len (OO0O0OO0OOOO000O0 [OOO0O0O0000OOO0OO ])):#line:37
+            OO0000O0OO000OOOO =OO0O0OO0OOOO000O0 [OOO0O0O0000OOO0OO ][O00O000000O00O000 ]if OO0O0OO0OOOO000O0 [OOO0O0O0000OOO0OO ][O00O000000O00O000 ]!=0 else '.'#line:38
+            if (OOO0O0O0000OOO0OO ,O00O000000O00O000 )in cut :#line:39
+                O0OOO000OOOOO0O0O .append (f"[{OO0000O0OO000OOOO}]")#line:40
             else :#line:41
-                OO0OO0O000O00O00O .append (f" {O0000O00O000OO0O0} ")#line:42
-        print (''.join (OO0OO0O000O00O00O ))#line:43
+                O0OOO000OOOOO0O0O .append (f" {OO0000O0OO000OOOO} ")#line:42
+        print (''.join (O0OOO000OOOOO0O0O ))#line:43
     print ()#line:44
-def inline (O0O0000O0O0OO00OO ,OO0000O00O0OO0OOO ):#line:47
-    if O0O0000O0O0OO00OO [0 ]==OO0000O00O0OO0OOO [0 ]:#line:48
+def inline (O0O0O00000O000OO0 ,OO00O0OOO0OOO0OOO ):#line:47
+    if O0O0O00000O000OO0 [0 ]==OO00O0OOO0OOO0OOO [0 ]:#line:48
         return True #line:49
-    if O0O0000O0O0OO00OO [1 ]==OO0000O00O0OO0OOO [1 ]:#line:50
+    if O0O0O00000O000OO0 [1 ]==OO00O0OOO0OOO0OOO [1 ]:#line:50
         return True #line:51
-    return abs (O0O0000O0O0OO00OO [0 ]-OO0000O00O0OO0OOO [0 ])==abs (O0O0000O0O0OO00OO [1 ]-OO0000O00O0OO0OOO [1 ])#line:52
-size ,colors =[int (OOOO0OOO00OO0000O )for OOOO0OOO00OO0000O in input ().split ()]#line:55
+    return abs (O0O0O00000O000OO0 [0 ]-OO00O0OOO0OOO0OOO [0 ])==abs (O0O0O00000O000OO0 [1 ]-OO00O0OOO0OOO0OOO [1 ])#line:52
+size ,colors =[int (OOO0O000000000000 )for OOO0O000000000000 in input ().split ()]#line:55
 grid =[[]]#line:57
 for _ in range (size ):#line:58
-    grid .append ([0 ]+[int (OOO00OO000OO0O000 )for OOO00OO000OO0O000 in input ().split ()])#line:59
+    grid .append ([0 ]+[int (O000O00OO0OO0000O )for O000O00OO0OO0000O in input ().split ()])#line:59
 n_pairs =int (input ())#line:62
 pairs =[]#line:63
 for _ in range (n_pairs ):#line:64
-    p =[int (OOOOOOO00O000O0O0 )for OOOOOOO00O000O0O0 in input ().split ()]#line:65
+    p =[int (O0OO00O0OOOO0O00O )for O0OO00O0OOOO0O00O in input ().split ()]#line:65
     pairs .append (((p [0 ],p [1 ]),(p [2 ],p [3 ])))#line:66
     if pairs [-1 ][0 ]==pairs [-1 ][1 ]:#line:67
         sys .exit (f"Invalid pair: identity ({pairs[-1][0]}, {pairs[-1][1]}) is not permitted.")#line:68
@@ -85,19 +86,22 @@ for step in range (len (pairs )):#line:77
             else :#line:97
                 cut .append ((x ,f [1 ]))#line:98
     else :#line:99
-        y =f [1 ]#line:100
-        dy =1 if f [1 ]<t [1 ]else -1 #line:101
-        for x in range (min (f [0 ],t [0 ])+1 ,max (f [0 ],t [0 ])):#line:102
-            y +=dy #line:103
-            if grid [x ][y ]not in (0 ,grid [f [0 ]][f [1 ]]):#line:104
-                sys .exit (f"Wrong pair: ({f}, {t}) but ({x}, {y}) has different color.")#line:105
-            else :#line:106
-                cut .append ((x ,y ))#line:107
-    pretty_print (step +1 ,grid ,cut )#line:109
-    for (x ,y )in cut :#line:110
-        grid [x ][y ]=0 #line:111
-for i in range (1 ,len (grid )):#line:113
-    for j in range (1 ,len (grid [i ])):#line:114
-        if grid [i ][j ]!=0 :#line:115
-            sys .exit (f"Wrong final state: button on ({i}, {j}).")#line:116
+        x =f [0 ]#line:100
+        y =f [1 ]#line:101
+        dx =1 if f [0 ]<t [0 ]else -1 #line:102
+        dy =1 if f [1 ]<t [1 ]else -1 #line:103
+        for _ in range (min (f [0 ],t [0 ])+1 ,max (f [0 ],t [0 ])):#line:104
+            x +=dx #line:105
+            y +=dy #line:106
+            if grid [x ][y ]not in (0 ,grid [f [0 ]][f [1 ]]):#line:107
+                sys .exit (f"Wrong pair: ({f}, {t}) but ({x}, {y}) has different color.")#line:108
+            else :#line:109
+                cut .append ((x ,y ))#line:110
+    pretty_print (step +1 ,grid ,cut )#line:112
+    for (x ,y )in cut :#line:113
+        grid [x ][y ]=0 #line:114
+for i in range (1 ,len (grid )):#line:116
+    for j in range (1 ,len (grid [i ])):#line:117
+        if grid [i ][j ]!=0 :#line:118
+            sys .exit (f"Wrong final state: button on ({i}, {j}).")#line:119
 print ("CORRECT!")
--- problem-3/checker.py
+++ problem-3/checker.py
@@ -23,85 +23,64 @@ Example input:
 3 3
 3 3
 """
-import fileinput #line:26
-import re #line:27
-import sys #line:28
-def pretty_print (O0000OO00OO0O00O0 ,OO0O0OO0OOOO000O0 ,cut =None ):#line:31
-    if cut is None :#line:32
-        cut =[]#line:33
-    print (f'#### STEP {O0000OO00OO0O00O0} ####')#line:34
-    for OOO0O0O0000OOO0OO in range (1 ,len (OO0O0OO0OOOO000O0 )):#line:35
-        O0OOO000OOOOO0O0O =[]#line:36
-        for O00O000000O00O000 in range (1 ,len (OO0O0OO0OOOO000O0 [OOO0O0O0000OOO0OO ])):#line:37
-            OO0000O0OO000OOOO =OO0O0OO0OOOO000O0 [OOO0O0O0000OOO0OO ][O00O000000O00O000 ]if OO0O0OO0OOOO000O0 [OOO0O0O0000OOO0OO ][O00O000000O00O000 ]!=0 else '.'#line:38
-            if (OOO0O0O0000OOO0OO ,O00O000000O00O000 )in cut :#line:39
-                O0OOO000OOOOO0O0O .append (f"[{OO0000O0OO000OOOO}]")#line:40
-            else :#line:41
-                O0OOO000OOOOO0O0O .append (f" {OO0000O0OO000OOOO} ")#line:42
-        print (''.join (O0OOO000OOOOO0O0O ))#line:43
-    print ()#line:44
-def inline (O0O0O00000O000OO0 ,OO00O0OOO0OOO0OOO ):#line:47
-    if O0O0O00000O000OO0 [0 ]==OO00O0OOO0OOO0OOO [0 ]:#line:48
-        return True #line:49
-    if O0O0O00000O000OO0 [1 ]==OO00O0OOO0OOO0OOO [1 ]:#line:50
-        return True #line:51
-    return abs (O0O0O00000O000OO0 [0 ]-OO00O0OOO0OOO0OOO [0 ])==abs (O0O0O00000O000OO0 [1 ]-OO00O0OOO0OOO0OOO [1 ])#line:52
-size ,colors =[int (OOO0O000000000000 )for OOO0O000000000000 in input ().split ()]#line:55
-grid =[[]]#line:57
-for _ in range (size ):#line:58
-    grid .append ([0 ]+[int (O000O00OO0OO0000O )for O000O00OO0OO0000O in input ().split ()])#line:59
-n_pairs =int (input ())#line:62
-pairs =[]#line:63
-for _ in range (n_pairs ):#line:64
-    p =[int (O0OO00O0OOOO0O00O )for O0OO00O0OOOO0O00O in input ().split ()]#line:65
-    pairs .append (((p [0 ],p [1 ]),(p [2 ],p [3 ])))#line:66
-    if pairs [-1 ][0 ]==pairs [-1 ][1 ]:#line:67
-        sys .exit (f"Invalid pair: identity ({pairs[-1][0]}, {pairs[-1][1]}) is not permitted.")#line:68
-    if pairs [-1 ][0 ][0 ]not in range (1 ,len (grid ))or pairs [-1 ][0 ][1 ]not in range (1 ,len (grid )):#line:69
-        sys .exit (f"Invalid pair: first element of ({pairs[-1][0]}, {pairs[-1][1]}) is out of range.")#line:70
-    if pairs [-1 ][1 ][0 ]not in range (1 ,len (grid ))or pairs [-1 ][1 ][1 ]not in range (1 ,len (grid )):#line:71
-        sys .exit (f"Invalid pair: second element of ({pairs[-1][0]}, {pairs[-1][1]}) is out of range.")#line:72
-    if not inline (pairs [-1 ][0 ],pairs [-1 ][1 ]):#line:73
-        sys .exit (f"Invalid pair: ({pairs[-1][0]}, {pairs[-1][1]}) is not an horizontal, vertical or diagonal line.")#line:74
-pretty_print (0 ,grid )#line:76
-for step in range (len (pairs )):#line:77
-    f ,t =pairs [step ]#line:78
-    if grid [f [0 ]][f [1 ]]==0 :#line:79
-        sys .exit (f"Wrong pair: ({f}, {t}) but no button on {f}.")#line:80
-    if grid [t [0 ]][t [1 ]]==0 :#line:81
-        sys .exit (f"Wrong pair: ({f}, {t}) but no button on {t}.")#line:82
-    if grid [f [0 ]][f [1 ]]!=grid [t [0 ]][t [1 ]]:#line:83
-        sys .exit (f"Wrong pair: ({f}, {t}) but {f} and {t} have different colors.")#line:84
-    cut =[f ,t ]#line:86
-    if f [0 ]==t [0 ]:#line:87
-        for x in range (min (f [1 ],t [1 ])+1 ,max (f [1 ],t [1 ])):#line:88
-            if grid [f [0 ]][x ]not in (0 ,grid [f [0 ]][f [1 ]]):#line:89
-                sys .exit (f"Wrong pair: ({f}, {t}) but ({f[0]}, {x}) has different color.")#line:90
-            else :#line:91
-                cut .append ((f [0 ],x ))#line:92
-    elif f [1 ]==t [1 ]:#line:93
-        for x in range (min (f [0 ],t [0 ])+1 ,max (f [0 ],t [0 ])):#line:94
-            if grid [x ][f [1 ]]not in (0 ,grid [f [0 ]][f [1 ]]):#line:95
-                sys .exit (f"Wrong pair: ({f}, {t}) but ({x}, {f[1]}) has different color.")#line:96
-            else :#line:97
-                cut .append ((x ,f [1 ]))#line:98
-    else :#line:99
-        x =f [0 ]#line:100
-        y =f [1 ]#line:101
-        dx =1 if f [0 ]<t [0 ]else -1 #line:102
-        dy =1 if f [1 ]<t [1 ]else -1 #line:103
-        for _ in range (min (f [0 ],t [0 ])+1 ,max (f [0 ],t [0 ])):#line:104
-            x +=dx #line:105
-            y +=dy #line:106
-            if grid [x ][y ]not in (0 ,grid [f [0 ]][f [1 ]]):#line:107
-                sys .exit (f"Wrong pair: ({f}, {t}) but ({x}, {y}) has different color.")#line:108
-            else :#line:109
-                cut .append ((x ,y ))#line:110
-    pretty_print (step +1 ,grid ,cut )#line:112
-    for (x ,y )in cut :#line:113
-        grid [x ][y ]=0 #line:114
-for i in range (1 ,len (grid )):#line:116
-    for j in range (1 ,len (grid [i ])):#line:117
-        if grid [i ][j ]!=0 :#line:118
-            sys .exit (f"Wrong final state: button on ({i}, {j}).")#line:119
+import fileinput #line:28
+import re #line:29
+import sys #line:30
+sym ={10 :'\u2500',11 :'\u2502',20 :'\u256E',21 :'\u256F',22 :'\u2570',23 :'\u256D',30 :'\u2576',31 :'\u2577',32 :'\u2574',33 :'\u2575',50 :'\u252C',51 :'\u2524',52 :'\u2534',53 :'\u251C',}#line:48
+def pretty_print (OO0OOO00O00O00OOO ,OO0O0O00OO00O0O00 ,cut =None ):#line:51
+    if len(sys.argv) >= 2 and sys.argv[1] == '--quiet': return
+    if cut is None :#line:52
+        cut =[]#line:53
+    print (f'#### STEP {OO0OOO00O00O00OOO} ####')#line:54
+    for OO000O00O00O00OO0 in range (len (OO0O0O00OO00O0O00 )):#line:55
+        OOO00O000000OO0O0 =[]#line:56
+        for O00O0OOOO000O0O0O in range (len (OO0O0O00OO00O0O00 [OO000O00O00O00OO0 ])):#line:57
+            OOO00O000000OO0O0 .append (sym [OO0O0O00OO00O0O00 [OO000O00O00O00OO0 ][O00O0OOOO000O0O0O ]])#line:58
+        print (''.join (OOO00O000000OO0O0 ))#line:59
+    print ()#line:60
+def inline (OOO0O00O0O0OO000O ,OO0O0OO0OOO0O0OO0 ):#line:63
+    if OOO0O00O0O0OO000O [0 ]==OO0O0OO0OOO0O0OO0 [0 ]:#line:64
+        return True #line:65
+    if OOO0O00O0O0OO000O [1 ]==OO0O0OO0OOO0O0OO0 [1 ]:#line:66
+        return True #line:67
+    return abs (OOO0O00O0O0OO000O [0 ]-OO0O0OO0OOO0O0OO0 [0 ])==abs (OOO0O00O0O0OO000O [1 ]-OO0O0OO0OOO0O0OO0 [1 ])#line:68
+size =int (input ())#line:72
+grid =[]#line:74
+for _ in range (size ):#line:75
+    grid .append ([int (OOOO0000OOO0O0O00 )for OOOO0000OOO0O0O00 in input ().split ()])#line:76
+n_rotations =int (input ())#line:79
+rotations =[]#line:80
+for _ in range (n_rotations ):#line:81
+    rotations .append ([int (O0O00000O00OOOO0O )for O0O00000O00OOOO0O in input ().split ()])#line:82
+    if rotations [-1 ][0 ]-1 not in range (len (grid ))or rotations [-1 ][1 ]-1 not in range (len (grid )):#line:83
+        sys .exit (f"Invalid rotation: {rotations[-1]} is out of range.")#line:84
+pretty_print (0 ,grid )#line:86
+for step in range (len (rotations )):#line:87
+    i ,j =rotations [step ]#line:88
+    grid [i -1 ][j -1 ]+=1 #line:89
+    if grid [i -1 ][j -1 ]==12 :#line:90
+        grid [i -1 ][j -1 ]=10 #line:91
+    elif grid [i -1 ][j -1 ]%10 ==4 :#line:92
+        grid [i -1 ][j -1 ]-=4 #line:93
+    pretty_print (step +1 ,grid )#line:94
+connections ={'left':[10 ,20 ,21 ,32 ,50 ,51 ,52 ],'right':[10 ,22 ,23 ,30 ,50 ,52 ,53 ],'up':[11 ,21 ,22 ,33 ,51 ,52 ,53 ],'down':[11 ,20 ,23 ,31 ,50 ,51 ,53 ],}#line:102
+seen =set ()#line:104
+stack =[(0 ,0 ,'nil')]#line:105
+while stack :#line:106
+    i ,j ,d =stack .pop ()#line:107
+    if i not in range (size )or j not in range (size ):#line:109
+        sys .exit (f"Water on ({i+1}, {j+1}), out of grid!")#line:110
+    if d !='nil'and grid [i ][j ]not in connections [d ]:#line:112
+        sys .exit (f"Wrong connection on ({i+1}, {j+1})")#line:113
+    if (i ,j )in seen :#line:115
+        continue #line:116
+    seen .add ((i ,j ))#line:117
+    if grid [i ][j ]in connections ['left']:#line:119
+        stack .append ((i ,j -1 ,'right'))#line:120
+    if grid [i ][j ]in connections ['right']:#line:121
+        stack .append ((i ,j +1 ,'left'))#line:122
+    if grid [i ][j ]in connections ['up']:#line:123
+        stack .append ((i -1 ,j ,'down'))#line:124
+    if grid [i ][j ]in connections ['down']:#line:125
+        stack .append ((i +1 ,j ,'up'))#line:126
 print ("CORRECT!")
